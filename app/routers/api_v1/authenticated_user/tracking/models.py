7cc47fd098757246fed271bda1ec776077788a5a
cro2003 <chiragrai2003@icloud.com>
2025-01-19 06:31:53 +0530

    Edited Models for Tracking

--- app/routers/api_v1/authenticated_user/tracking/models.py
+++ app/routers/api_v1/authenticated_user/tracking/models.py
@@ -12,11 +12,11 @@ class DailyTrackingData(BaseModel):
     id: Annotated[str | None, Field(default=None, alias="_id")]
     datentime: Annotated[str, Field(max_length=30, description="Date and Time")]
     diet: Optional[List[Meal]] = Field(description="List of Meal", default=[])
-    weight: Annotated[float, Field(ge=1, description="Weight in KG")]
+    weight: Annotated[float, Field(ge=0, description="Weight in KG")]
     temperature: Annotated[float, Field(ge=1, description="Temperature in Celsius")]
-    water_intake: Optional[float] = Field(ge=1, description="Water Intake in litre", default=None)
+    water_intake: Optional[float] = Field(ge=0, description="Water Intake in litre", default=None)
     walking: Optional[float] = Field(ge=0, description="Walking in KM", default=None)
     behavior: Annotated[str, Field(max_length=30, description="Behavior of the Pet")]
-    mood_indicator: Annotated[int, Field(ge=1, description="Mood Indicator from 3")]
+    mood_indicator: Annotated[int, Field(ge=1, le=3, description="Mood Indicator from 3")]
     sleep_time: Optional[float] = Field(ge=0, description="Sleeping Time in Hour", default=None)
     notes: Annotated[str, Field(max_length=100, description="Notes")]
